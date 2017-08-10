c65c6cce5bddd6ec7880259ececa6b7092de634f
tmartinez <tmartinez@switchfly.com>
2017-08-10 12:10:38 -0700

    adding enzyme to test components easily

--- src/routes/App/App.test.js
+++ src/routes/App/App.test.js
@@ -1,19 +1,28 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import appFactory from "./App";
+import React from 'react';
+import {shallow} from 'enzyme';
+import appFactory from './App';
 
-// let App;
-// let TodoStore;
+let App;
 
-// beforeEach(function () {
-//   // AppDispatcher = require('../../dispatcher/AppDispatcher');
-//   TodoStore = require('../../stores/PomodoroStore');
-//   // callback = AppDispatcher.register.mock.calls[0][0];
-//
-//   App = appFactory(React, TodoStore, null, null, null, null);
-// });
+const pomodoroStoreStub = {
+    get: function () {
+        return {
+            totalTime: 0,
+            timeLeft: 0
+        };
+    },
 
-xit('renders without crashing', () => {
-    // const div = document.createElement('div');
-    // ReactDOM.render(<App />, div);
+    addChangeListener: jest.fn()
+};
+
+describe('App', () => {
+
+    beforeEach(function () {
+        App = appFactory(React, pomodoroStoreStub, {}, 'div', 'div');
+    });
+
+    it('renders without crashing', () => {
+        shallow(<App/>);
+    });
+
 });
