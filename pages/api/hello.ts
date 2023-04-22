16ad770316680497c93153e939157b0f51edec10
tonymtz <atony.mtz@gmail.com>
2023-04-22 15:49:56 -0700

    adds sentry reporting

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler (
+const handler = (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+  res: NextApiResponse<Data>,
+) => {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default withSentry(handler)
