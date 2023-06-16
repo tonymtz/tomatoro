f099050cfd3e98207bfcb946ed4c13471016dc23
tonymtz <atony.mtz@gmail.com>
2023-06-16 15:33:26 -0700

    updates sentry

--- next.config.js
+++ next.config.js
@@ -26,6 +26,28 @@ const nextConfig = {
 
 module.exports = withSentryConfig(
   nextConfig,
-  { silent: true },
-  { hideSourceMaps: true },
+  {
+    silent: true,
+    org: "tonymtz",
+    project: "tomatoro-com",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  },
 );
